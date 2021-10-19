ee6f77e8bd6b36a0941ad587f4ad9801a814cc7d
Ben Brady <benbradybusiness@gmail.com>
2021-10-20 00:00:49 +0100

    Setup only creates tables if they don't exist

--- setup.py
+++ setup.py
@@ -10,37 +10,35 @@ load_dotenv()
 #               Databases              #
 # ------------------------------------ #
 
-print("\033[1;31m [WARNING: DELETES ALL SAVED DATA] \033[0m")
+print("\033[1;31m Checking Databases \033[0m")
 # Choice = input("Regenerate Databases (y/n):")
-Choice = 'y'
-if Choice.lower() == 'y':
-    DatabaseStore = Path('./Data/Database.db')
-    if DatabaseStore.exists():
-        os.remove(DatabaseStore)
+DatabaseStore = Path('./Data/Database.db')
+if DatabaseStore.exists():
+    os.remove(DatabaseStore)
 
-    with sqlite3.connect(DatabaseStore) as conn:
-        conn.execute("PRAGMA foreign_keys = ON")
-        conn.execute("""
-            CREATE TABLE GUILDS(
-                    ID     INT  PRIMARY KEY NOT NULL,
-                    Settings    TEXT        NOT NULL
-                );""")
+with sqlite3.connect(DatabaseStore) as conn:
+    conn.execute("PRAGMA foreign_keys = ON")
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS GUILDS(
+                ID     INT  PRIMARY KEY NOT NULL,
+                Settings    TEXT        NOT NULL
+            );""")
 
-        conn.execute("""
-            CREATE TABLE MESSAGES(
-                    ID          INT  PRIMARY KEY  NOT NULL,
-                    GuildID     INT               NOT NULL,
-                    FOREIGN KEY(GuildID)
-                        REFERENCES GUILDS(ID)
-                );""")
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS MESSAGES(
+                ID          INT  PRIMARY KEY  NOT NULL,
+                GuildID     INT               NOT NULL,
+                FOREIGN KEY(GuildID)
+                    REFERENCES GUILDS(ID)
+            );""")
 
-        conn.execute("""
-            CREATE TABLE HASHES(
-                    MsgID   INT             NOT NULL,
-                    Hash    BLOB            NOT NULL,
-                    Type    TEXT            NOT NULL,
-                    FOREIGN KEY(MsgID)
-                        REFERENCES MESSAGES(ID)
-                );""")
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS HASHES(
+                MsgID   INT             NOT NULL,
+                Hash    BLOB            NOT NULL,
+                Type    TEXT            NOT NULL,
+                FOREIGN KEY(MsgID)
+                    REFERENCES MESSAGES(ID)
+            );""")
 
-    print("    Databases Regenerated")
+print("    Databases Regenerated")
