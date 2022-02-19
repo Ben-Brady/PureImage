590f5f2777dae9c1bb98cc9e16a31cf99ff4e484
Ben Brady <benbradybusiness@gmail.com>
2022-02-19 23:06:35 +0000

    Imrpvoed annotations in ImageHash

--- src/Modules/ImageHash.py
+++ src/Modules/ImageHash.py
@@ -8,22 +8,20 @@ Image.MAX_IMAGE_PIXELS = 10_000_000
 Log = Logger.Get("ImageHash")
 
 
-def Hash(img: bytes) -> list:
+def Hash(img: bytes) -> bytes:
     IMG = Image.open(io.BytesIO(img))
     PHash = im.phash(IMG)
-
     return bytes.fromhex(str(PHash))
 
 
-# TODO: Mew .10 feature: int.count_bits(), use with XOR for hamming distance
-def Distance(Hash: bytes, Other: bytes) -> int:
-    Hash = list(Hash)
-    Other = list(Other)
-    if len(Hash) != len(Other):
+def Distance(a: bytes, b: bytes) -> int:
+    a_list:list[int] = list(a)
+    b_list:list[int] = list(b)
+    if len(a_list) != len(b_list):
         raise OverflowError("Byte lengths do not match")
 
     Difference = 0
-    for x, y in zip(Hash, Other):
+    for x, y in zip(a_list, b_list):
         XOR = x ^ y
         for bit in range(8):
             Difference += (XOR >> bit) & 1
